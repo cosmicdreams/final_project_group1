fd5c7d437cb655b8b04a5671688b17f630367a2b
chris.weber <chris.weber@nerdery.com>
2022-06-03 15:04:30 -0500

    Week 4: I shouldn't put quotes around colors.

--- app/static/js/pokemon-card/pokemon-card.js
+++ app/static/js/pokemon-card/pokemon-card.js
@@ -16,24 +16,25 @@ export class PokemonCard extends LitElement {
 
     static styles = css`
     :host {
-        --bug: "#26de81";
-        --dragon: "#ffeaa7";
-        --electric: "#fed330";
-        --fairy: "#FF0069";
-        --fighting: "#30336b";
-        --fire: "#f0932b";
-        --flying: "#81ecec";
-        --grass: "#00b894";
-        --ground: "#EFB549";
-        --ghost: "#a55eea";
-        --ice: "#74b9ff";
-        --normal: "#95afc0";
-        --poison: "#6c5ce7";
-        --psychic: "#a29bfe";
-        --rock: "#2d3436";
-        --water: "#0190FF";
+        --bug: #26de81;
+        --dragon: #ffeaa7;
+        --electric: #fed330;
+        --fairy: #FF0069;
+        --fighting: #30336b;
+        --fire: #f0932b;
+        --flying: #81ecec;
+        --grass: #00b894;
+        --ground: #EFB549;
+        --ghost: #a55eea;
+        --ice: #74b9ff;
+        --normal: #95afc0;
+        --poison: #6c5ce7;
+        --psychic: #a29bfe;
+        --rock: #2d3436;
+        --water: #0190FF;
         --default: green;
-        
+    }
+    :host {
         position: relative;      
         width: 100%;
         padding: 30px 20px;
@@ -135,7 +136,7 @@ export class PokemonCard extends LitElement {
           <img src=${this.imgSrc} alt="${this.pokeName}"/>
           <h2 class="poke-name">${this.pokeName}</h2>
           <div class="types">
-
+            <span style="background-color: var(--${this.poketype}, black)">${this.poketype}</span>
           </div>
           <div class="stats">
             <div>
