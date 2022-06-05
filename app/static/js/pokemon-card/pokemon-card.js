d785e4d3a8c65294c71a6a0673774c9f40a7650a
Terra Lasho <96261773+Beetleee@users.noreply.github.com>
2022-06-04 20:16:27 -0500

    Update pokemon-card.js

--- app/static/js/pokemon-card/pokemon-card.js
+++ app/static/js/pokemon-card/pokemon-card.js
@@ -45,9 +45,7 @@ export class PokemonCard extends LitElement {
     }
     :host([poketype=electric]) {
         background: radial-gradient(circle at 50% 0%, var(--electric) 35%, #fed330 36%);
-    }
-    }
-    :host([poketype=pyschic]) {
+    }([poketype=pyschic]) {
         background: radial-gradient(circle at 50% 0%, var(--psychic) 35%, #a29bfe 36%);
     } 
     img {
